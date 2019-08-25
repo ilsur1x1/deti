a2e9b1c984521123d4d219dec7e6a542c9843f52
ilsur1x1 <54503217+ilsur1x1@users.noreply.github.com>
2019-08-25 17:28:12 +0300

    Create konkurs.js

---
+++ konkurs.js
@@ -0,0 +1,33 @@
+
+var numbers = 
+    [
+  
+      
+129,
+117, 
+46,
+130,
+63,
+57,
+82,
+96,
+126,
+37,
+       
+]
+
+var last_index = 0
+
+function getTrueRandomInteger(min,max){
+
+document.getElementById("true-random-integer-generator-result").innerHTML='<img src="/util/cp/images/ajax-loader.gif" alt="Loading..." />';
+
+setTimeout(function(){
+
+document.getElementById("true-random-integer-generator-result").innerHTML=numbers[last_index];
+
+last_index += 1;
+
+}, 700); 
+
+}
